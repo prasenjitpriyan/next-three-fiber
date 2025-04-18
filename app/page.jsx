a10286b3cfebcbe66c2db18d62533b210e3c98bb
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-04-18 19:07:18 +0530

    Basic canvas added

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
+'use client'
+
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <Canvas>
+      <mesh>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshPhongMaterial />
+      </mesh>
+      <ambientLight intensity={0.1} />
+      <directionalLight position={[0, 0, 5]} color="red" />
+    </Canvas>
+  )
 }
 
 export default HomePage
